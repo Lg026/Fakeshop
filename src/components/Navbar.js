d867c56affb8694754c8069a4e262c1c0fc5691c
Lg026 <130603739+Lg026@users.noreply.github.com>
2023-09-22 22:41:14 -0700

    commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,3 @@
-// import React from 'react'
-// import { NavLink } from 'react-router-dom'
-// import navStyles from '../styles/nav.module.css'
-
-// export const Navbar = () => {
-//   return (
-//     <header>
-//         <nav className={navStyles.nav}>
-//             <ul className={navStyles.links}>
-//                 <li><NavLink exact activeClassName={navStyles.onActive} className={navStyles.a} to='/'>Home</NavLink></li>
-//                 <li><NavLink exact activeClassName={navStyles.onActive} className={navStyles.a} to='/about'>About</NavLink></li>
-//                 <li><NavLink exact activeClassName={navStyles.onActive} className={navStyles.a} to='/contact'>Contact</NavLink></li>
-//                 <li><NavLink className={navStyles.cart} to='/cart'><i className="fa fa-shopping-cart"></i> </NavLink></li>
-//             </ul>
-//         </nav>
-//     </header>
-//   )
-// }
-
-// export default {Navbar}
-
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import navStyles from '../styles/nav.module.css'
--- src/index.js
+++ src/index.js
@@ -3,18 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { AppProvider } from './components/ProductContext';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
     <AppProvider>
       <App />
     </AppProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
